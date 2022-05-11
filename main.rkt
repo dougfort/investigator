ee80fd146bdd99120b783be15be26cb1e800c294
dougfort <doug.fort@gmail.com>
2022-05-11 15:46:18 -0400

    get bytes from message body

--- main.rkt
+++ main.rkt
@@ -2,6 +2,7 @@
 
 (require net/http-client)
 (require racket/string)
+(require racket/port)
 
 (define (load-bearer-token path)
   (define in-port (open-input-file "/home/dougfort/.config/investigator/token"))
@@ -13,8 +14,12 @@
   (define auth-header (format "Authorization: Bearer ~a" bearer-token))
   (define hc (http-conn-open "api.twitter.com"  #:ssl? #t))
   (http-conn-send! hc "https://api.twitter.com/2/tweets/search/recent?query=from:twitterdev" #:headers (list auth-header))
-  (call-with-values (λ () (http-conn-recv! hc)) (λ (status-line headers data-port) (println headers)))
-  (http-conn-close! hc)) 
+  (let-values ([(status-line headers data-port) (http-conn-recv! hc)])
+    (cond
+      [(equal? status-line #"HTTP/1.1 200 OK")
+       (port->bytes data-port #:close? #t)]
+      [else (error "Invalid HTTP status: " status-line)])
+    )) 
 
 (module* main #f
   (define bearer-token (load-bearer-token "/home/dougfort/.config/investigator/token"))
