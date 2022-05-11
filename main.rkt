d4f360232f811cff25e748b69cf21bff4e5516b6
dougfort <doug.fort@gmail.com>
2022-05-11 09:49:39 -0400

    initial contact with Twitter API
    read bearer token from file
    use http-sendrecv to poll Twitter

--- main.rkt
+++ main.rkt
@@ -1,50 +1,22 @@
 #lang racket/base
 
-(module+ test
-  (require rackunit))
+(require net/http-client)
+(require racket/string)
 
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;; For your convenience, we have included LICENSE-MIT and LICENSE-APACHE files.
-;; If you would prefer to use a different license, replace those files with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
+(define (load-bearer-token path)
+  (define in-port (open-input-file "/home/dougfort/.config/investigator/token"))
+  (let ([line (read-line in-port)])
+    (close-input-port in-port)
+    (string-trim line)))
 
-;; Code here
+(define (read-from-api bearer-token)
+  (let ([auth-header (format "Authorization: Bearer ~a" bearer-token)])
+    (http-sendrecv
+     "api.twitter.com"
+     "https://api.twitter.com/2/tweets/search/recent?query=from:twitterdev"
+     #:ssl? #t
+     #:headers (list auth-header))))
 
-
-
-(module+ test
-  ;; Any code in this `test` submodule runs when this file is run using DrRacket
-  ;; or with `raco test`. The code here does not run when this file is
-  ;; required by another module.
-
-  (check-equal? (+ 2 2) 4))
-
-(module+ main
-  ;; (Optional) main submodule. Put code here if you need it to be executed when
-  ;; this file is run using DrRacket or the `racket` executable.  The code here
-  ;; does not run when this file is required by another module. Documentation:
-  ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
-
-  (require racket/cmdline)
-  (define who (box "world"))
-  (command-line
-    #:program "my-program"
-    #:once-each
-    [("-n" "--name") name "Who to say hello to" (set-box! who name)]
-    #:args ()
-    (printf "hello ~a~n" (unbox who))))
+(module* main #f
+  (define bearer-token (load-bearer-token "/home/dougfort/.config/investigator/token"))
+  (call-with-values (lambda () (read-from-api bearer-token)) (lambda (a b c) (println a))))
