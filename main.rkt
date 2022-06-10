f37e8dd0919b04032ba0f15c0ff825bdfdaaa0cd
dougfort <doug.fort@gmail.com>
2022-06-10 11:13:24 -0400

    add some query restrictions

--- main.rkt
+++ main.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require net/http-client)
+(require net/uri-codec)
 (require racket/string)
 (require json)
 
@@ -10,11 +11,14 @@
     (close-input-port in-port)
     (string-trim line)))
 
+(define (prepare-query raw-query)
+  (format "~a lang:en -is:retweet" raw-query))
+
 (define (read-from-api bearer-token query)
   (define auth-header (format "Authorization: Bearer ~a" bearer-token))
   (define hc (http-conn-open "api.twitter.com"  #:ssl? #t))
   (http-conn-send! hc
-                   (format "https://api.twitter.com/2/tweets/search/recent?query=~a" query)
+                   (format "https://api.twitter.com/2/tweets/search/recent?query=~a" (uri-encode query))
                    #:headers (list auth-header))
   (let-values ([(status-line headers data-port) (http-conn-recv! hc)])
     (cond
@@ -29,9 +33,15 @@
           (hash-ref meta-ht 'oldest_id)
           (hash-ref meta-ht 'newest_id)))
 
+(define (report-data data)
+  (for ([item data])
+    (printf "~v: ~v~n" (hash-ref item 'id) (hash-ref item 'text))))
+
 (module* main #f
+  (print "query: ")
+  (define raw-query (read-line))
   (define bearer-token (load-bearer-token "/home/dougfort/.config/investigator/token"))
-  (define json-ht (read-from-api bearer-token "from:twitterdev"))
+  (define json-ht (read-from-api bearer-token (prepare-query raw-query)))
   ; we expect a hash table with these keys: '(data meta)
   (report-meta (hash-ref json-ht 'meta))
-  (hash-ref json-ht 'data))
+  (report-data (hash-ref json-ht 'data)))
